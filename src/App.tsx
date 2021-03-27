9db3ba312639f7d12180475d174f6420ec1f8f99
puneeth <puneethmotukuru@gmail.com>
2021-03-27 21:51:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'antd/dist/antd.css'; 
+import {FormData} from "./components/FormData"
+function App() {
+  return(
+    <div>
+      <FormData />
+    </div>
+  )
+}
+export default App;
